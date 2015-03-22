d98762c4510f1592efb883b1b591b83b1cafa670
restu <restu.suhendar@gmail.com>
2015-03-22 21:56:04 +0700

    add spring MVC framework

---
+++ src/main/java/com/aarestu/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.aarestu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(ModelMap model) {
+		model.addAttribute("message", "Hello Spring WEB MVC!");
+		return "hello";
+	}
+
+}
